a241da58d07691e52f98da35bc4e8e8d56a091f0
fernandaljb <fer.jammal@gmail.com>
2026-03-14 11:35:24 -0300

    feat: defini a interface dos clientes e das serviceOrders

---
+++ src/types/client.ts
@@ -0,0 +1,8 @@
+// eu criei essa interface baseada no swager https://trainee.fidelis.workers.dev/docs
+export interface Client {
+  id: number;
+  name: string;
+  phone: string;
+  email: string;
+  created_at: string;
+}
---
+++ src/types/serviceOrder.ts
@@ -0,0 +1,8 @@
+export interface ServiceOrder {
+  id: number;
+  client_id: string;
+  device: string;
+  isue: string;
+  status: string;
+  created_at: string;
+}
